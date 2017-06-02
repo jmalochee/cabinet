7037d6d61fa0451e18e7b8e8dcda1969781c02cc
jmalochee <jmalochee@gmail.com>
2017-06-02 16:55:28 -0400

    initial placeholders

---
+++ react/src/components/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Router } from 'react-router';
+
+class Index extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selected_button: null
+    }
+
+  }
+
+  render() {
+    return(
+      <div className="main-index">
+
+      </div>
+    )
+  }
+}
